60042bbc6fd51fb8769cbe046abf41d8a94cddc2
Markus Fanebust Dregi <mfane@statoil.com>
2017-07-07 15:23:34 +0200

    Add setup.py and a test

---
+++ src/turbine_polynomial/turbine_regression.py
@@ -0,0 +1,86 @@
+import pandas, numpy
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+
+FEATURES = ["AIR_IN_TEMP", "AIR_IN_PRES", "DISCHARGE_TEMP", "DISCHARGE_PRES"]
+TARGET = "SIMULATED_EFF"
+
+def load_data(filename):
+    data = pandas.read_csv(filename, sep=";", header=0)
+    data["TIME"] = pandas.to_datetime(data["TIME"])
+
+    return data
+
+def preprocess_data(data):
+    data = data.dropna(axis=0, how="any")
+    data = data[data['DISCHARGE_PRES'] > 6]
+    data = data[(0.8 <= data['AIR_IN_PRES']) & (data['AIR_IN_PRES'] <= 1.1)]
+    data = data.reset_index()
+
+    return data
+
+def split_data_set(data, training_fraction = 0.9):
+    split_time = data["TIME"][int(len(data)*training_fraction)]
+    data = data.set_index("TIME")
+
+    X = data[FEATURES]
+    y = data[TARGET]
+
+    training_data = (X[:split_time:], y[:split_time:])
+    test_data = (X[split_time::], y[split_time::])
+
+    return (X, y), training_data, test_data
+
+def polynomialize_data(data_sets, degree=2):
+    data_transformer = lambda X : PolynomialFeatures(degree=degree).fit_transform(X)
+    return [(data_transformer(data[0]), data[1]) for data in data_sets]
+
+def linear_regression(X, y):
+    reg = linear_model.LinearRegression()
+    reg = reg.fit(X, y)
+
+    return reg
+
+def visualize(data, training_data, test_data, reg_mod):
+    data_timeline = data[1].index
+    training_data_timeline = training_data[1].index
+    test_data_timeline = test_data[1].index
+
+    plt.plot(
+            data_timeline, data[1], "ro",
+            training_data_timeline, reg_mod.predict(training_data[0]), "bo",
+            test_data_timeline, reg_mod.predict(test_data[0]), "go"
+            )
+
+    plt.show()
+
+def evaluate(data, training_data, test_data, reg_mod):
+
+    print "Score on training set %s" % reg_mod.score(*training_data)
+    print "Score on test set %s" %  reg_mod.score(*test_data)
+
+    visualize(data, training_data, test_data, reg_mod)
+
+if __name__ == "__main__":
+
+    data = load_data("../../data/turbin_data.csv")
+    print " >> Loaded %d data points" % len(data)
+
+    data = preprocess_data(data)
+    print " >> After preprocessing %d data points remaining" % len(data)
+
+    data, training_data, test_data = split_data_set(data, training_fraction=0.6)
+
+    [data, training_data, test_data] = polynomialize_data([data, training_data, test_data], degree=2)
+
+    reg_mod = linear_regression(*training_data)
+
+    evaluate(
+            data,
+            training_data,
+            test_data,
+            reg_mod
+            )
