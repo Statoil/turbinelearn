13ed818fcc5a2c77ad3c766a7c7262fa0900c619
Erik Parmann <eparmann@gmail.com>
2017-07-10 15:27:53 +0200

    Add crossvalidation across files
    Add the option of doing crossvalidation across files, with some files
    as training and others are test.
    Also added a parser for commandline arguments

--- src/turbine_polynomial/turbine_regression.py
+++ src/turbine_polynomial/turbine_regression.py
@@ -9,22 +9,26 @@ from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
+from itertools import combinations
+import argparse
+
 from sklearn.decomposition import PCA
 
-DATASET = ["data/LOCO_B_HGA.csv",
-           "data/LOCO_B_HGB.csv",
-           "data/LOCO_B_HTA.csv",
-           "data/LOCO_B_HTB.csv",
-           "data/LOCO_C_HGA.csv",
-           "data/LOCO_C_HGB.csv",
-           "data/LOCO_C_HTA.csv",
-           "data/LOCO_C_HTB.csv"]
+DATASET_HT = ["data/LOCO_B_HTA.csv",
+              "data/LOCO_B_HTB.csv",
+              "data/LOCO_C_HTA.csv",
+              "data/LOCO_C_HTB.csv"]
+DATASET_HG = ["data/LOCO_B_HGA.csv",
+              "data/LOCO_B_HGB.csv",
+              "data/LOCO_C_HGA.csv",
+              "data/LOCO_C_HGB.csv"]
+DATASET = DATASET_HG + DATASET_HT
 
-LIMITS =   {'SPEED'         : (5000, 10000),
-            'DISCHARGE_PRES': (5, 20),
-            'SIMULATED_EFF' : (0, 100),
-            'AIR_IN_PRES'   : (0.8, 1.1),
-            'DISCHARGE_TEMP': (0, 600)}
+LIMITS =   {'SPEED'          : (5000, 10000),
+            'DISCHARGE_PRES' : (5, 20),
+            'SIMULATED_EFF'  : (0, 100),
+            'AIR_IN_PRES'    : (0.8, 1.1),
+            'DISCHARGE_TEMP' : (0, 600)}
 
 feature_map = {
     "Air In.Phase - Temperature.Overall.Overall"        : "AIR_IN_TEMP",
@@ -47,12 +51,18 @@ def detect_outliers(data):
     data['OUTLIER'] = Z
 
 
-def load_data(filename):
+def load_single_file(filename):
     data = pandas.read_csv(filename, sep=";", header=0)
     data = data.rename(columns=feature_map)
     data["TIME"] = pandas.to_datetime(data["TIME"])
-
     return data
+
+
+def load_data(files):
+    if (isinstance(files,basestring)):
+        files = [files]
+    return pandas.concat(map(load_single_file, files))
+
 
 def normalize_column(data, field):
     if field not in data:
@@ -62,27 +72,28 @@ def normalize_column(data, field):
     data[field] -= min_
     data[field] /= dif_
 
+
 def preprocess_data(data, normalize=(), limits={}):
     for key in limits:
         min_, max_ = limits[key]
         data = data[(min_ <= data[key]) & (data[key] <= max_)]
-
     data = data[FEATURES + [TARGET, "TIME", 'SPEED']]
     data = data.dropna(axis=0, how="any")
     data = data.reset_index()
 
     for field in normalize:
         normalize_column(data, field)
+    data = data.set_index("TIME")
     return data
+
 
 def split_data_set(data, training_fraction = 0.9):
     split_index = int(len(data)*training_fraction)
 
     if split_index >= len(data):
         return [[]] * 3
-
-    split_time = data["TIME"][split_index]
-    data = data.set_index("TIME")
+    # This is probably a bit inefficient
+    split_time = data.reset_index()["TIME"][split_index]
 
     X = data[FEATURES]
     y = data[TARGET]
@@ -92,15 +103,17 @@ def split_data_set(data, training_fraction = 0.9):
 
     return (X, y), training_data, test_data
 
+
 def polynomialize_data(data_sets, degree=2):
     data_transformer = lambda X : PolynomialFeatures(degree=degree).fit_transform(X)
     return [(data_transformer(data[0]), data[1]) for data in data_sets]
 
+
 def linear_regression(X, y):
     reg = linear_model.LinearRegression()
     reg = reg.fit(X, y)
-
     return reg
+
 
 def visualize(data, training_data, test_data, reg_mod):
     data_timeline = data[1].index
@@ -111,8 +124,7 @@ def visualize(data, training_data, test_data, reg_mod):
         data_timeline, data[1], "ro",
         training_data_timeline, reg_mod.predict(training_data[0]), "bo",
         test_data_timeline, reg_mod.predict(test_data[0]), "go",
-        markersize=2
-    )
+        markersize=2)
 
 
 def evaluate(data, training_data, test_data, reg_mod):
@@ -139,10 +151,8 @@ def main(data_files, test_data_files=None, training_fraction=0.9, degree=1, limi
         print(" >> Using  %d / %d data points after preprocessing (deleted %d points)" %
               (len(data), N, N-len(data)))
 
-        data, training_data, test_data = split_data_set(
-                                                    data,
-                                                    training_fraction=training_fraction
-                                                    )
+        data, training_data, test_data = split_data_set(data,
+                                                        training_fraction=training_fraction)
 
         print(" >> Training on %d, testing on %d" % (training_data[0].shape[0], test_data[0].shape[0]))
         if not (data and training_data and test_data):
@@ -161,9 +171,48 @@ def main(data_files, test_data_files=None, training_fraction=0.9, degree=1, limi
     return dataset
 
 
+def read_and_split_files(data_files,training_fraction=0.9, degree=1):
+    data = load_data(data_files)
+    print(" >> Loaded %d data points" % len(data))
+
+    data = preprocess_data(data, limits=LIMITS)
+    print(" >> After preprocessing %d data points remaining" % len(data))
+    if training_fraction > 0:
+        data, training_data, test_data = split_data_set(data, training_fraction=training_fraction)
+        if not (data and training_data and test_data):
+            print(" >> Did not have enough data to do regression")
+            return
+        return polynomialize_data([data, training_data, test_data], degree=degree)
+    else:
+        X = data[FEATURES]
+        y = data[TARGET]
+        data=(X,y)
+        return polynomialize_data([data], degree=degree)
+
+
+def train_and_evaluate_single_file(data_file, training_fraction=0.9, degree=1):
+    [data, training_data, test_data] = read_and_split_files(data_file, training_fraction=training_fraction, degree=degree)
+    reg_mod = linear_regression(*training_data)
+    evaluate(
+            data,
+            training_data,
+            test_data,
+            reg_mod
+            )
+    visualize(data, training_data, test_data, reg_mod)
+
+
+def individual_cross_validation(data_file, k=5, degree=1):
+    [data] = read_and_split_files(data_file, training_fraction=0, degree=degree)
+    #Split dataset into 5 consecutive folds (without shuffling by default).
+    scores = cross_val_score(linear_model.LinearRegression(), *data, cv = k )
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    print("Scores:%s"%str(scores))
+
+
 def pca(data_file):
     data = load_data(data_file)
-    data = preprocess_data(data)
+    data = preprocess_data(data, limits=LIMITS)
     X = data[FEATURES + [TARGET]]
     y = data[TARGET]
     pca = PCA(n_components=2)
@@ -180,34 +229,77 @@ def pca(data_file):
     points = plt.scatter(X_1, X_2, c=y, cmap=cmap)
 
 
-def individual_cross_validation(data_files, training_fraction=0.9, degree=1):
-    data = load_data(data_files)
-    print(" >> Loaded %d data points" % len(data))
 
-    data = preprocess_data(data)
-    print(" >> After preprocessing %d data points remaining" % len(data))
-    # This sets data on a (X,y) format. We ignore the training and test_data split
-    data, training_data, test_data = split_data_set(
-                                                    data,
-                                                    training_fraction=training_fraction
-                                                    )
-    [data] = polynomialize_data([data], degree=degree)
+def filebased_cross_validation(data_files, test_data_files, degree=1):
+    print("\nTraining on data_files %s " % ", ".join(data_files))
 
-    scores = cross_val_score(linear_model.LinearRegression(), *data, cv = 5 )
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    print(scores)
+    [data] = read_and_split_files(data_files, training_fraction=0, degree=degree)
+    reg_mod = linear_regression(*data)
+
+    print("Testing on test_data_files %s" % ", ".join(test_data_files))
+    for input_file in test_data_files:
+        [data] = read_and_split_files(input_file,training_fraction=0, degree=degree)
+        score = reg_mod.score(*data)
+        print("Score on %s is %.4f" % (input_file, score))
+
+
+def file_cross_val(data_files,k=2,degree=2):
+    in_size = len(data_files)
+    data_files=set(data_files)
+    for training_set in combinations(data_files,in_size-k):
+        test_set = data_files.difference(training_set)
+        filebased_cross_validation(training_set,test_set,degree=degree)
+
 
 if __name__ == "__main__":
-    plt.title('Turbine polynomial')
-    i = 0
-    for input_file in DATASET:
-        fname = path_split(input_file)
-        i = i + 1
-        if '--pca' in argv:
+    parser = argparse.ArgumentParser(description='Learns a polynomial.')
+    parser.add_argument('--method', dest='method',choices=['fcv', 'icv', 'simple', 'pca', 'all'], required=True,
+                        help='Uses either filebased cross validation (fcv), individual file cross validation (icv), run a single train-test run on each file (simple) or do PCA (pca)')
+    parser.add_argument('--degree', dest='degree',type=int, choices=range(1, 10), default=2,
+                        help='The degree of the polynomial to train, defaults to 2')
+
+    parser.add_argument('--training-fraction', dest='training_fraction',type=float, default=0.6,
+                        help='The fraction of data to use as training data, only useed when method=simple')
+    parser.add_argument('--k', dest='k',type=int, choices=range(1, 10), default=2,
+                        help='The number of folds to keep out when using cross validation. For method=fcv it is the number of files to keep out')
+
+    args = parser.parse_args()
+
+    degree = args.degree
+    training_fraction = args.training_fraction
+    k = args.k
+
+    if args.method == 'fcv':
+        file_cross_val(DATASET,k=k,degree=degree)
+    if args.method == 'icv':
+        for input_file in DATASET:
+            print("\nDoing individual cross validation for %s" % input_file)
+            individual_cross_validation(input_file, degree=degree)
+    if args.method == 'simple':
+        for input_file in DATASET:
+            print("\nDoing regression for %s" % input_file)
+            train_and_evaluate_single_file(input_file,
+                                           training_fraction = training_fraction,
+                                           degree=degree)
+
+
+    if args.method == "pca":
+        plt.title('Turbine polynomial PCA')
+        i = 0
+        for input_file in DATASET:
+            fname = path_split(input_file)
+            i = i + 1
             plt.subplot(4, 2, i)
             plt.ylabel(fname[1])
             pca(input_file)
-        else:
+        plt.show()
+
+    if args.method == 'all':
+        plt.title('Turbine polynomial')
+        i = 0
+        for input_file in DATASET:
+            fname = path_split(input_file)
+            i = i + 1
             plt.subplot(8, 2, i)
             plt.ylabel(fname[1])
             print("\nDoing regression for %s" % input_file)
@@ -217,8 +309,4 @@ if __name__ == "__main__":
             plt.subplot(8, 2, i)
             plt.ylim([0, 20*1000])
             plt.plot(data['TIME'], X, 'o', markersize=2)
-    plt.show()
-
-    for input_file in DATASET:
-        print("\nDoing cross validation for %s" % input_file)
-        individual_cross_validation(input_file, training_fraction=0.6, degree=2)
+        plt.show()
