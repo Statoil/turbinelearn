d4260200403e4a58c62ccaec8678a525cc84b01c
Pål Grønås Drange <PGDR@statoil.com>
2017-07-11 12:39:00 +0200

    Rename Python module to turbinelearn

--- python/turbinelearn/__init__.py
+++ python/turbinelearn/__init__.py
@@ -11,5 +11,5 @@ __status__ = ''
 
 __ALL__  = ['load_data']
 __ALL__ += ['pca', 'regression', 'file_cross_val', 'individual_cross_validation']
-__ALL__  = ['train_and_evaluate_single_file']
+__ALL__ += ['train_and_evaluate_single_file']
 __ALL__ += ['FEATURES', 'TARGET', 'LIMITS']
