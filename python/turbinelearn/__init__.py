cf8d78ebfcb2f30c211883fab699f88ec3a891f8
Pål Grønås Drange <PGDR@statoil.com>
2017-07-11 13:14:02 +0200

    Extract file and dataset functionality to its own file

--- python/turbinelearn/__init__.py
+++ python/turbinelearn/__init__.py
@@ -1,8 +1,10 @@
-from .turbine_regression import load_data
+from .turbine_file import (enum_files, load_single_file, load_data,
+                           normalize_column, preprocess_data, split_data_set)
+from .turbine_file import FEATURES, TARGET, LIMITS
+
 from .turbine_regression import file_cross_val, individual_cross_validation
 from .turbine_regression import train_and_evaluate_single_file
 from .turbine_regression import regression, pca
-from .turbine_regression import FEATURES, TARGET, LIMITS
 
 __version__ = '0.0.1'
 __copyright__ = 'Copyright 2017, Statoil ASA'
--- python/turbinelearn/turbine_regression.py
+++ python/turbinelearn/turbine_regression.py
@@ -1,5 +1,4 @@
 from __future__ import print_function, absolute_import, division
-import pandas
 
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
@@ -9,24 +8,9 @@ from sklearn.decomposition import PCA
 
 from itertools import combinations, combinations_with_replacement
 
-
-LIMITS =   {'SPEED'          : (5000, 10000),
-            'DISCHARGE_PRES' : (5, 20),
-            'SIMULATED_EFF'  : (0, 100),
-            'AIR_IN_PRES'    : (0.8, 1.1),
-            'DISCHARGE_TEMP' : (0, 600)}
-
-feature_map = {
-    "Air In.Phase - Temperature.Overall.Overall"        : "AIR_IN_TEMP",
-    "Air In.Phase - Pressure.Overall.Overall"           : "AIR_IN_PRES",
-    "Discharge.Phase - Temperature.Overall.Overall"     : "DISCHARGE_TEMP",
-    "Discharge.Phase - Pressure.Overall.Overall"        : "DISCHARGE_PRES",
-    "K-100.Polytropic Efficiency.Polytropic Efficiency" : "SIMULATED_EFF",
-    "GAS GENERATOR SPEED"                               : "SPEED"
-}
-
-FEATURES = ["AIR_IN_TEMP", "AIR_IN_PRES", "DISCHARGE_TEMP", "DISCHARGE_PRES"]
-TARGET = "SIMULATED_EFF"
+from .turbine_file import (enum_files, load_single_file, load_data,
+                           normalize_column, preprocess_data, split_data_set,
+                           FEATURES, TARGET, LIMITS)
 
 
 def detect_outliers(data):
@@ -35,61 +19,6 @@ def detect_outliers(data):
     clf.fit(X)
     Z = clf.decision_function(X)
     data['OUTLIER'] = Z
-
-
-def load_single_file(filename):
-    data = pandas.read_csv(filename, sep=";", header=0)
-    data = data.rename(columns=feature_map)
-    data["TIME"] = pandas.to_datetime(data["TIME"])
-    # Marks the dataset as comming from a variable (HG) or full (HT) -speed turbine
-    data["TURBINE_TYPE"] = 0 if "HG" in filename.upper() else 1
-    return data
-
-
-def load_data(files):
-    if (isinstance(files,basestring)):
-        files = [files]
-    return pandas.concat(map(load_single_file, files))
-
-
-def normalize_column(data, field):
-    if field not in data:
-        return
-    min_, max_ = min(data[field]), max(data[field])
-    dif_ = max_ - min_
-    data[field] -= min_
-    data[field] /= dif_
-
-
-def preprocess_data(data, normalize=(), limits={}):
-    for key in limits:
-        min_, max_ = limits[key]
-        data = data[(min_ <= data[key]) & (data[key] <= max_)]
-    data = data[FEATURES + [TARGET, "TIME", 'SPEED']]
-    data = data.dropna(axis=0, how="any")
-    data = data.reset_index()
-
-    for field in normalize:
-        normalize_column(data, field)
-    data = data.set_index("TIME")
-    return data
-
-
-def split_data_set(data, training_fraction = 0.9):
-    split_index = int(len(data)*training_fraction)
-
-    if split_index >= len(data):
-        return [[]] * 3
-    # This is probably a bit inefficient
-    split_time = data.reset_index()["TIME"][split_index]
-
-    X = data[FEATURES]
-    y = data[TARGET]
-
-    training_data = (X[:split_time:], y[:split_time:])
-    test_data = (X[split_time::], y[split_time::])
-
-    return (X, y), training_data, test_data
 
 
 def polynomialize_data(data_sets, degree=2):
@@ -222,9 +151,6 @@ def filebased_cross_validation(data_files, test_data_files, degree=1):
         print("Score on %s is %.4f" % (input_file, score))
 
 
-def file_cross_val(data_files,k=2,degree=2):
-    in_size = len(data_files)
-    data_files=set(data_files)
-    for training_set in combinations(data_files,in_size-k):
-        test_set = data_files.difference(training_set)
-        filebased_cross_validation(training_set,test_set,degree=degree)
+def file_cross_val(data_files, k=2, degree=2):
+    for training_set, test_set in enum_files(data_files, k):
+        filebased_cross_validation(training_set, test_set, degree=degree)
